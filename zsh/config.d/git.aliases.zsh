24d2dfb0f984b52a0af741e4f0a13df33e956ccf
Aaron Kollasch <aaron@kollasch.dev>
2023-02-16 01:00:13 -0500

    add more git log aliases

--- zsh/config.d/git.aliases.zsh
+++ zsh/config.d/git.aliases.zsh
@@ -78,7 +78,9 @@ alias ggs='gg --stat'
 alias ggup='git log --branches --not --remotes --no-walk --decorate --oneline' # FROM https://stackoverflow.com/questions/39220870/in-git-list-names-of-branches-with-unpushed-commits
 alias gll='git log --graph --pretty=oneline --abbrev-commit'
 alias gll1='git log --graph --pretty=oneline --abbrev-commit -n 10'
+alias gl1=gll1
 alias gll2='git log --graph --pretty=oneline --abbrev-commit -n 20'
+alias gl2=gll2
 alias gnew='git log HEAD@{1}..HEAD@{0}' # Show commits since last pull, see http://blogs.atlassian.com/2014/10/advanced-git-aliases/
 alias gwc='git whatchanged'
 alias glt='git log --oneline --decorate --graph --all'
