16561c8d7abe39055529b4e66e8a327d74c0d98e
Aaron Kollasch <aaron@kollasch.dev>
2023-03-06 01:28:24 -0500

    move nvim textobject plugins to separate file

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -145,41 +145,6 @@ return {
     -- better <leader>p
     { "inkarkat/vim-ReplaceWithRegister", event = "VeryLazy" },
 
-    -- textobjects, see https://github.com/kana/vim-textobj-user/wiki
-    { "kana/vim-textobj-line",        dependencies = "kana/vim-textobj-user", event = "VeryLazy" }, -- al/il for the current line
-    { "kana/vim-textobj-indent",      dependencies = "kana/vim-textobj-user", event = "VeryLazy" }, -- ai/ii for a block of similarly indented lines / aI/iI for a block of lines with the same indentation
-    { "kana/vim-textobj-entire",      dependencies = "kana/vim-textobj-user", event = "VeryLazy" }, -- ae/ie for the entire region of the current buffer
-    { "sgur/vim-textobj-parameter",   dependencies = "kana/vim-textobj-user", event = "VeryLazy" }, -- a,/i, for an argument to a function
-    { "vimtaku/vim-textobj-keyvalue", dependencies = "kana/vim-textobj-user", event = "VeryLazy" }, -- ak/ik and av/iv for key/value
-    { "glts/vim-textobj-comment",     dependencies = "kana/vim-textobj-user", event = "VeryLazy" }, -- ac/ic for a comment
-    { "jceb/vim-textobj-uri",         dependencies = "kana/vim-textobj-user", event = "VeryLazy" }, -- au/iu for a URI, also adds go to open URL
-    {
-        "preservim/vim-textobj-sentence",                                                           -- as/is for the current sentence (replaces built-in objects/motions)
-        dependencies = "kana/vim-textobj-user",
-        event = "VeryLazy",
-        config = function ()
-            vim.fn["textobj#sentence#init"]()
-        end
-    },
-    {
-        "Julian/vim-textobj-variable-segment",                                                      -- aS/iS for a subword, separated by camelCase or underscore
-        dependencies = "kana/vim-textobj-user",
-        event = "VeryLazy",
-        init = function()
-            vim.g.loaded_textobj_variable_segment = 1
-        end,
-        config = function()
-            vim.cmd([[
-            call textobj#user#plugin('variable', {
-                \ '-': {
-                \     'sfile': expand('<sfile>:p'),
-                \     'select-a': 'aS',  'select-a-function': 'textobj#variable_segment#select_a',
-                \     'select-i': 'iS',  'select-i-function': 'textobj#variable_segment#select_i',
-                \ }})
-            ]])
-        end
-    },
-
     -- colorscheme
     "sainnhe/edge",
     { "aaronkollasch/darcula", ft = "python", dev = true }, -- fork of doums/darcula
