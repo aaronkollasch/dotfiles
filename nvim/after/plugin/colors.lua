8908892965b948b8dfb38f08776182c09aebe2fd
Aaron Kollasch <aaron@kollasch.dev>
2023-03-07 19:35:04 -0500

    add links for new nvim semantic highlighting

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -107,3 +107,26 @@ else
   colorscheme edge
 endif
 ]])
+
+-- link new semantic highlighting groups
+-- see https://github.com/neovim/neovim/pull/22022
+-- see https://gist.github.com/swarn/fb37d9eefe1bc616c2a7e476c0bc0316
+local links = {
+  ['@lsp.type.namespace'] = '@namespace',
+  ['@lsp.type.type'] = '@type',
+  ['@lsp.type.class'] = '@type',
+  ['@lsp.type.enum'] = '@type',
+  ['@lsp.type.interface'] = '@type',
+  ['@lsp.type.struct'] = '@structure',
+  ['@lsp.type.parameter'] = '@parameter',
+  ['@lsp.type.variable'] = '@variable',
+  ['@lsp.type.property'] = '@property',
+  ['@lsp.type.enumMember'] = '@constant',
+  ['@lsp.type.function'] = '@function',
+  ['@lsp.type.method'] = '@method',
+  ['@lsp.type.macro'] = '@macro',
+  ['@lsp.type.decorator'] = '@function',
+}
+for newgroup, oldgroup in pairs(links) do
+  vim.api.nvim_set_hl(0, newgroup, { link = oldgroup, default = true })
+end
