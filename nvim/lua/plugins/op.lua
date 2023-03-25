37335dbbe1d54a18218243f7d3d72fc059b494f0
Aaron Kollasch <aaron@kollasch.dev>
2023-03-25 11:15:17 -0400

    add octo.nvim to nvim

--- nvim/lua/plugins/op.lua
+++ nvim/lua/plugins/op.lua
@@ -1,24 +1,40 @@
 return {
-    "mrjones2014/op.nvim",
-    build = "make install",
-    keys = {
-        {
-            "<leader>op",
-            function()
-                require("op").op_insert()
-            end,
-            desc = "[O]ne[P]assword",
+    {
+        "mrjones2014/op.nvim",
+        build = "make install",
+        keys = {
+            {
+                "<leader>op",
+                function()
+                    require("op").op_insert()
+                end,
+                desc = "[O]ne[P]assword",
+            },
+            -- { "<leader>op", function() require("op.sidebar").toggle end, desc = "[O]ne[P]assword" },
         },
-        -- { "<leader>op", function() require("op.sidebar").toggle end, desc = "[O]ne[P]assword" },
+        opts = {
+            sidebar = {
+                side = "left",
+            },
+            mappings = {
+                ["q"] = function()
+                    require("op.sidebar").close()
+                end,
+            },
+        },
     },
-    opts = {
-        sidebar = {
-            side = "left",
-        },
-        mappings = {
-            ["q"] = function()
-                require("op.sidebar").close()
+    {
+        'pwntester/octo.nvim',
+        cmd = "Octo",
+        opts = {
+            gh_env = function ()
+                -- see https://blog.1password.com/1password-neovim/
+                local github_token = require('op.api').item.get({ 'GitHub Personal Access Token', '--fields', 'token' })[1]
+                if not github_token or not vim.startswith(github_token, 'ghp_') then
+                    error('Failed to get GitHub token.')
+                end
+                return { GITHUB_TOKEN = github_token }
             end,
         },
-    },
+    }
 }
