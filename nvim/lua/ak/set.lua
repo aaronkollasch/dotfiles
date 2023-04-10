0d3e371d9b0330803f81f01c5407972c21a45e8b
Aaron Kollasch <aaron@kollasch.dev>
2023-04-10 11:19:25 -0400

    hide language providers from nvim checkhealth

--- nvim/lua/ak/set.lua
+++ nvim/lua/ak/set.lua
@@ -58,3 +58,9 @@ vim.opt.complete = ".,w,b,u,t,i,kspell"
 vim.opt.cmdheight = 0
 
 vim.opt.shortmess:append({ I = true })
+
+-- hide language providers from checkhealth
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
