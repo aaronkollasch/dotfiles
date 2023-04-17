4286ddbb6ffdfb31c3c47179c244c6df39874a17
Aaron Kollasch <aaron@kollasch.dev>
2023-04-17 10:20:34 -0400

    add tmexclude alias for macos

--- zsh/config.d/mac.zsh
+++ zsh/config.d/mac.zsh
@@ -3,3 +3,4 @@ alias ldd='otool -L "$@"'
 alias jitouch-gesture-stats="rg -or '\$1' 'Gesture \"(.*?)\"' ~/Library/Logs/com.jitouch.Jitouch.log | sort | uniq -c | sort -nr"
 alias brewdeps="brew leaves | xargs brew deps --include-build --tree"  # https://stackoverflow.com/questions/41029842/easy-way-to-have-homebrew-list-all-package-dependencies
 alias btmdump="sudo sfltool dumpbtm > ~/Documents/btmdump.text"  # https://eclecticlight.co/2023/02/15/controlling-login-and-background-items-in-ventura
+alias tmexclude="sudo tmutil addexclusion \"$@\""
