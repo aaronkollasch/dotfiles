dcc83b735f2dd6f98bbd7673b27379bb26453a97
Aaron Kollasch <aaron@kollasch.dev>
2023-05-21 11:57:44 -0400

    swap harpoon popup keys

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -6,10 +6,10 @@ return {
     -- stylua: ignore
     keys = {
         { "<leader>a", function() require("harpoon.mark").add_file() end, desc = "[A]dd to Harpoon" },
-        { "<C-s>", function()
+        { "<leader><C-s>", function()
             require("telescope").extensions.harpoon.marks(require("telescope.themes").get_dropdown({}))
         end, desc = "Find harpoon files" },
-        { "<leader><C-s>", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle Harpoon menu" },
+        { "<C-s>", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle Harpoon menu" },
         { "<C-n>", function() require("harpoon.ui").nav_file(1) end, desc = "Goto Harpoon file 1" },
         { "<C-,>", function() require("harpoon.ui").nav_file(2) end, desc = "Goto Harpoon file 2" },
         { "<C-.>", function() require("harpoon.ui").nav_file(3) end, desc = "Goto Harpoon file 3" },
