21b848dd0ab691e210899b2ac97e7b1927429ed1
Aaron Kollasch <aaron@kollasch.dev>
2025-07-28 08:14:23 -0400

    add ruff isort to ruff python formatting

--- nvim/lua/ak/lsp/init.lua
+++ nvim/lua/ak/lsp/init.lua
@@ -134,7 +134,7 @@ require("conform").setup({
         lua = { "stylua" },
         python = function(bufnr)
             if require("conform").get_formatter_info("ruff_format", bufnr).available then
-                return { "ruff_format" }
+                return { "ruff_organize_imports", "ruff_format" }
             else
                 return { "isort", "black" }
             end
