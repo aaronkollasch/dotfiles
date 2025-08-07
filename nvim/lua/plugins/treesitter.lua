c01b566ba989d2a74cbf1f4b32d1e44224f9e1bf
Aaron Kollasch <aaron@kollasch.dev>
2025-08-07 08:12:39 -0400

    disable treesitter in tmux.conf files
    support for shell commands is lacking

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -31,6 +31,8 @@ local ts_opts = {
         -- `false` will disable the whole extension
         enable = true,
 
+        disable = { "tmux" },
+
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
